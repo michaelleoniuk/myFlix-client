782fb7772aa07087c26e2e0b38efcc3d18a18878
Michael Leoniuk <147387018+michaelleoniuk@users.noreply.github.com>
2024-01-02 20:42:54 +0100

    Update src/components/movie-card/movie-card.jsx
    Co-authored-by: StanleeO <stanlee.okwii@gmail.com>

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -5,7 +5,7 @@ import Card from "react-bootstrap/Card";
 export const MovieCard = ({ movie, onMovieClick }) => {
     return (
         <Card>
-            <Card.Img variant="top" src="https://cache.pressmailing.net/thumbnail/story_hires/c3822451-8a37-4c19-a495-fd3fc0edeb33/image.jpg" />
+            <Card.Img variant="top"  height={300} src="https://cache.pressmailing.net/thumbnail/story_hires/c3822451-8a37-4c19-a495-fd3fc0edeb33/image.jpg" />
             <Card.Body>
                 <Card.Title>{movie.Title}</Card.Title>
                 <Card.Text>{movie.Director.Name}</Card.Text>
